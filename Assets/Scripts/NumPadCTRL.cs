536609da9a77b37e480bb0492221814eee40f062
nablam <lamribenn@gmail.com>
2019-08-27 10:10:37 -0400

    hooked up the numpad events and tested on device

---
+++ Assets/Scripts/NumPadCTRL.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class NumPadCTRL : MonoBehaviour
+{
+    public void ReadButtonTouch(int argnum)
+    {
+        Debug.Log("pressed " + argnum);
+    }
+    public void UnTouch(int argnum)
+    {
+        Debug.Log("UNpressed " + argnum);
+
+    }
+}
---
+++ Assets/TouchManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+
+    public static bool guiTouch = false;
+    public void TouchInput(Collider2D collider) {
+        if (Input.touchCount > 0) { TouchPhases_0(collider); }
+        if (Input.touchCount > 1) { TouchPhases_1(collider); }
+    }
+
+    void TouchPhases_0(Collider2D collider) {
+        if (collider == Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position)) )
+        {
+            switch (Input.GetTouch(0).phase)
+            {
+                case TouchPhase.Began:
+                    SendMessage("OnFirstTouchBegan", SendMessageOptions.DontRequireReceiver);
+                    SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+                    guiTouch = true;
+                    break;
+
+                case TouchPhase.Stationary:
+                    SendMessage("OnFirstTouchStayed", SendMessageOptions.DontRequireReceiver);
+                    SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+                    guiTouch = true;
+                    break;
+                case TouchPhase.Moved:
+                    SendMessage("OnFirstTouchMoved", SendMessageOptions.DontRequireReceiver);
+                    SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+                    guiTouch = true;
+                    break;
+                case TouchPhase.Ended:
+                    SendMessage("OnFirstTouchEnded", SendMessageOptions.DontRequireReceiver);
+                    guiTouch = false;
+                    break;
+            }
+        }
+    }
+    void TouchPhases_1(Collider2D collider)
+    {
+        if (collider == Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position)))
+        {
+            switch (Input.GetTouch(0).phase)
+            {
+                case TouchPhase.Began:
+                    SendMessage("OnFirstTouchBegan", SendMessageOptions.DontRequireReceiver);
+                    SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+                    break;
+
+                case TouchPhase.Stationary:
+                    SendMessage("OnFirstTouchStayed", SendMessageOptions.DontRequireReceiver);
+                    SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+                    break;
+                case TouchPhase.Moved:
+                    SendMessage("OnFirstTouchMoved", SendMessageOptions.DontRequireReceiver);
+                    SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+                    break;
+                case TouchPhase.Ended:
+                    SendMessage("OnFirstTouchEnded", SendMessageOptions.DontRequireReceiver);
+                    break;
+            }
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
