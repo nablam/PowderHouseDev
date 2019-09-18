e23811562d3bcf6e45836c1a37267ddb8415a66c
nablam <lamribenn@gmail.com>
2019-09-18 19:43:24 -0400

    nav game start2

---
+++ Assets/#Navgame/NavGameScripts/NavGameFloorBuilder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class NavGameFloorBuilder : MonoBehaviour
+{
+
+    public GameObject FloorTileObj;
+    public int Width;
+    public int Height;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
