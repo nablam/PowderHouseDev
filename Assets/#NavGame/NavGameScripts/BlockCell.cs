fcdc4b2d6335dc8fe72a1283756ac77fb107485f
nablam <lamribenn@gmail.com>
2019-09-20 12:16:17 -0400

    ready to test slow generation for 21 by 11

---
+++ Assets/#NavGame/NavGameScripts/BlockCell.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class BlockCell : MonoBehaviour
+{
+    public TMPro.TextMeshPro m_Text;
+    public GameObject MyTile;
+    Renderer MyTileRenderer;
+    public void UpdateBlockText(string argTxt) { m_Text.text = argTxt; }
+    public void UpdateBlockTileMaterial(Material argMat) { MyTile.GetComponent<Renderer>().material = argMat; }
+    public SubBlockVisualCtrl MySubBlock;
+    float _blockEdgeLength;
+    public float BlockEdgeLength { get => _blockEdgeLength; set => _blockEdgeLength = value; }
+    NavMeshObstacle _MyObstacle;
+
+
+
+    public List<Transform> Adjacents;
+    public Vector3 Position;
+    public int Weight;
+    public Transform here;
+    public int AdjacentsOpened = 0;
+
+    private void OnEnable()
+    {
+        _MyObstacle = this.transform.GetComponentInChildren<NavMeshObstacle>();
+        _blockEdgeLength = _MyObstacle.gameObject.transform.localScale.x;
+    }
+    public void Set_ObstacleUpDown(bool argUP)
+    {
+        if (argUP)
+        {
+            _MyObstacle.gameObject.transform.Translate(Vector3.up * _blockEdgeLength);
+        }
+        else
+            _MyObstacle.gameObject.transform.Translate(Vector3.down * _blockEdgeLength);
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/#NavGame/NavGameScripts/RepoMaster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class RepoMaster : MonoBehaviour
+{
+    public static RepoMaster Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+            Destroy(gameObject);
+    }
+
+
+    public Material BlackMat;
+    public Material WhiteMat;
+    public Material YellowMat;
+    public Material RedMat;
+    public Material GreenMatLight;
+    public Material GreenMatMid;
+    public Material GreenMatDark;
+    public Material[] LightTiles = new Material[6];
+    public Material[] DarkTiles = new Material[6];
+
+}
