369dbe3d650b429111a370734f7d976ce6db7fda
nablam <lamribenn@gmail.com>
2019-12-11 21:41:17 -0500

    audio sync quick hack

---
+++ Assets/#BellHopGame/BellHopAudio/AudioSpectrum.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AudioSpectrum : MonoBehaviour
+{
+    float[] m_audioSpectrum;
+    public static float spectrumValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_audioSpectrum = new float[128];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming);
+        if (m_audioSpectrum != null && m_audioSpectrum.Length > 0)
+        {
+            spectrumValue = m_audioSpectrum[0] * 100;
+        }
+
+    }
+}
